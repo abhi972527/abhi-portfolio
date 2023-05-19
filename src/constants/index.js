534e543a0cd68dca0043d306918e6e22e4163e06
Abhijeet Kumar <akc972527@gmail.com>
2023-05-19 19:29:49 +0530

    added some scrolling feature in navbar

--- src/constants/index.js
+++ src/constants/index.js
@@ -40,10 +40,10 @@ import {
 } from "../assets";
 
 export const navLinks = [
-    {
-        id: "",
-        title: "Home",
-    },
+    // {
+    //     id: "",
+    //     title: "Home",
+    // },
     {
         id: "about",
         title: "About",
